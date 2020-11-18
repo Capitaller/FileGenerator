1b5b6e801c8663992d0eda00fab446ef32f8a972
Capitaller <44471056+Capitaller@users.noreply.github.com>
2020-11-18 09:08:05 +0200

    FileGenerator
    Create TXT files by Random

---
+++ FileGenerator/FileGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"BST165.txt";
+            int size = 165;
+            int[] array = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 99);
+            }
+            File.WriteAllText(Path, string.Join(" ", array));
+            Console.WriteLine("Comlete!");
+            //string Path = @"Queue100k.txt";
+            //int[] array = { 0 };
+            //array = File.ReadAllText(Path).Split().Select(int.Parse).ToArray();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    one.add(array[i]);
+            //}
+        }
+    }
+}
